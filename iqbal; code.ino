c514d37a0392b72d5c4e493a63170b3bca76106c
com1412 <85876082+com1412@users.noreply.github.com>
2022-12-18 02:40:05 +0530

    Update iqbal; code.ino

--- iqbal; code.ino
+++ iqbal; code.ino
@@ -68,3 +68,25 @@ Blynk.run();
 timer.run();
 sendTemps();
 }
+if (sensorState == 1 && lastState == 0) {
+  Serial.println("needs water, send notification");
+  Blynk.notify("Water your plants");
+  lastState = 1;
+  delay(1000);
+//send notification
+    
+  } 
+  else if (sensorState == 1 && lastState == 1) {
+    //do nothing, has not been watered yet
+  Serial.println("has not been watered yet");
+  delay(1000);
+  }
+  else {
+    //st
+    Serial.println("does not need water");
+    lastState = 0;
+    delay(1000);
+  }
+  
+  delay(100);
+}
